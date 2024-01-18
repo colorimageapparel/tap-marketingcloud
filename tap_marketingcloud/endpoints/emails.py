f50e49fda42ded7402f82f4389c15cd154c286fc
Chintan Patel <chintan.patel@aloyoga.com>
2024-01-18 15:01:42 +0530

    chore: make emails incremental

--- tap_marketingcloud/endpoints/emails.py
+++ tap_marketingcloud/endpoints/emails.py
@@ -4,8 +4,9 @@ import singer
 
 from tap_marketingcloud.client import request
 from tap_marketingcloud.dao import (DataAccessObject, exacttarget_error_handling)
+from tap_marketingcloud.pagination import get_date_page, before_date, increment_date
 from tap_marketingcloud.state import incorporate, save_state, \
-    get_last_record_value_for_table
+    get_last_record_value_for_table, get_end_date
 
 LOGGER = singer.get_logger()
 
@@ -36,31 +37,48 @@ class EmailDataAccessObject(DataAccessObject):
 
         search_filter = None
         # pass config to return start date if not bookmark is found
-        retrieve_all_since = get_last_record_value_for_table(self.state, table, self.config)
+        start = get_last_record_value_for_table(self.state, table, self.config)
+        end_date = get_end_date(self.config)
 
-        if retrieve_all_since is not None:
-            search_filter = {
-                'Property': 'ModifiedDate',
-                'SimpleOperator': 'greaterThan',
-                'Value': retrieve_all_since
-            }
+        if start is None:
+            raise RuntimeError('start_date not defined!')
 
-        stream = request('Email',
-                         selector,
-                         self.auth_stub,
-                         search_filter,
-                         batch_size=self.batch_size)
+        pagination_unit = self.config.get(
+            'pagination__{}_interval_unit'.format(table), 'minutes')
+        pagination_quantity = self.config.get(
+            'pagination__{}_interval_quantity'.format(table), 60)
 
-        catalog_copy = copy.deepcopy(self.catalog)
+        unit = {pagination_unit: int(pagination_quantity)}
 
-        for email in stream:
-            email = self.filter_keys_and_parse(email)
+        end = increment_date(start, unit)
 
-            self.state = incorporate(self.state,
-                                     table,
-                                     'ModifiedDate',
-                                     email.get('ModifiedDate'))
+        LOGGER.info('Before date: %s', before_date(start, end_date))
 
-            self.write_records_with_transform(email, catalog_copy, table)
+        while before_date(start, end_date):
+            LOGGER.info('Start: %s, end: %s, date comparison: %s', start, end_date, before_date(start, end_date))
+            LOGGER.info("Fetching {} from {} to {}"
+                        .format(table, start, end))
 
-        save_state(self.state)
+            search_filter = get_date_page('ModifiedDate', start, unit)
+
+            stream = request('Email',
+                                selector,
+                                self.auth_stub,
+                                search_filter,
+                                batch_size=self.batch_size)
+
+            catalog_copy = copy.deepcopy(self.catalog)
+
+            for event in stream:
+                event = self.filter_keys_and_parse(event)
+
+                self.state = incorporate(self.state, table, 'ModifiedDate', event.get('ModifiedDate'))
+
+                self.write_records_with_transform(event, catalog_copy, table)
+
+            self.state = incorporate(self.state, table, 'ModifiedDate', start)
+
+            save_state(self.state)
+
+            start = end
+            end = increment_date(start, unit)
